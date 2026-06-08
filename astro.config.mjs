dcc11bf8b6da269008f1e203b0c876c51f73cb87
Giacomo Fornari <gfornari@surveymonkey.com>
2026-06-08 11:24:53 +0200

    Fix Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gfornari.github.io',
-	base: '/the-context-window',
+	base: '/the-context-window/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
